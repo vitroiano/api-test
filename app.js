68fd08c437beef6f4f909c4ad04f81fd6daa357b
Vinicius Trainotti <vtrainotti@live.com>
2020-03-09 15:16:34 -0300

    Adicionado CORS

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false})); // apenas dados simples
+app.use(bodyParser.urlencoded({ extended: false })); // apenas dados simples
 app.use(bodyParser.json()); // somente json
+
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Content-Type, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+
+    next();
+
+});
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
